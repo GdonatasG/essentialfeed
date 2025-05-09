2a4d680a68a513fa12b3f7d37d93bedafbb0c215
donataszitkus <zitkusd@gmail.com>
2025-05-09 17:42:18 +0300

    Create FeedItem model

---
+++ EssentialFeed2/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed2
+//
+//  Created by Donatas Zitkus on 09/05/2025.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
