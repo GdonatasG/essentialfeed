4b578dfce3c5291e999c620a9079cf5a3e19ee1b
donataszitkus <zitkusd@gmail.com>
2025-05-10 16:10:01 +0300

    'RemoteFeedLoader' doesn't load data upon creation

--- EssentialFeed2Tests/RemoteFeedLoaderTests.swift
+++ EssentialFeed2Tests/RemoteFeedLoaderTests.swift
@@ -5,4 +5,21 @@
 //  Created by Donatas Zitkus on 10/05/2025.
 //
 
-import Foundation
+import XCTest
+
+class RemoteFeedLoader {
+    func load(){}
+}
+
+class HTTPClient {
+    var requestedURL: String?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    func test_init(){
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}
