159e14922e6f873db53b875c96e5f8805c601752
donataszitkus <zitkusd@gmail.com>
2025-05-24 18:07:43 +0300

    Extract cache deletion side effect on retrieval error from the load method to validate cache method

--- EssentialFeed2/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed2/Feed Cache/LocalFeedLoader.swift
@@ -45,10 +45,15 @@ public final class LocalFeedLoader {
             case .empty:
                 completion(.success([]))
             case let .failure(error):
-                self.store.deleteCachedFeed { _ in }
                 completion(.failure(error))
             }
         }
+    }
+    
+    public func validateCache() {
+        store.retrieve { _ in }
+        self.store.deleteCachedFeed { _ in }
+
     }
     
     private var maxCacheAgeInDays: Int {
--- EssentialFeed2Tests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed2Tests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -16,6 +16,15 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [])
     }
     
+    func test_validate_deletesCacheOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        
+        sut.validateCache()
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
     // MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
         let store = FeedStoreSpy()
@@ -25,5 +34,9 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         trackForMemoryLeak(sut, file: file, line: line)
         
         return (sut, store)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 1)
     }
 }
