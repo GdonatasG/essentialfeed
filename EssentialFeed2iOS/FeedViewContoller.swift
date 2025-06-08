53a9b68e387081e3335d56605f548325b72597dd
donataszitkus <zitkusd@gmail.com>
2025-06-08 16:09:37 +0300

    Move 'FeedViewController' to production

---
+++ EssentialFeed2iOS/FeedViewContoller.swift
@@ -0,0 +1,51 @@
+//
+//  FeedViewContoller.swift
+//  EssentialFeed2iOS
+//
+//  Created by Donatas Zitkus on 08/06/2025.
+//
+
+import Foundation
+import EssentialFeed2
+import UIKit
+
+public final class FeedViewController: UITableViewController {
+    let mainLoadingIndicator = UIActivityIndicatorView(style: .large)
+    private var loader: FeedLoader?
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainLoadingIndicator.center = view.center
+        view.addSubview(mainLoadingIndicator)
+        
+        let control = UIRefreshControl()
+        refreshControl = control
+        refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        tableView.setContentOffset(CGPoint(x: 0, y: -refreshControl!.frame.size.height), animated: false)
+        
+        mainLoadingIndicator.startAnimating()
+        load { [weak self] in
+            self?.mainLoadingIndicator.stopAnimating()
+        }
+    }
+    
+    @objc private func refresh(){
+        refreshControl?.beginRefreshing()
+        load { [weak self] in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+    
+    @objc private func load(completion: @escaping () -> Void) {
+        loader?.load { [weak self] result in
+            guard let _ = self else { return }
+            completion()
+        }
+    }
+}
--- EssentialFeed2iOSTests/FeedViewControllerTests.swift
+++ EssentialFeed2iOSTests/FeedViewControllerTests.swift
@@ -7,48 +7,7 @@
 
 import XCTest
 import UIKit
-import EssentialFeed2
-
-final class FeedViewController: UITableViewController {
-    let mainLoadingIndicator = UIActivityIndicatorView(style: .large)
-    private var loader: FeedLoader?
-    
-    convenience init(loader: FeedLoader) {
-        self.init()
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        mainLoadingIndicator.center = view.center
-        view.addSubview(mainLoadingIndicator)
-        
-        let control = UIRefreshControl()
-        refreshControl = control
-        refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        tableView.setContentOffset(CGPoint(x: 0, y: -refreshControl!.frame.size.height), animated: false)
-        
-        mainLoadingIndicator.startAnimating()
-        load { [weak self] in
-            self?.mainLoadingIndicator.stopAnimating()
-        }
-    }
-    
-    @objc private func refresh(){
-        refreshControl?.beginRefreshing()
-        load { [weak self] in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-    
-    @objc private func load(completion: @escaping () -> Void) {
-        loader?.load { [weak self] result in
-            guard let _ = self else { return }
-            completion()
-        }
-    }
-}
+import EssentialFeed2iOS
 
 final class FeedViewControllerTests: XCTestCase {
     func test_loadFeedActions_requestFeedFromLoader() {
