80797fe412b5414d9cb2e08f9cd92c6178a8db4c
donataszitkus <zitkusd@gmail.com>
2025-06-28 07:35:33 +0300

    Create extension method on 'FeedImageDataCache' to clarify save intent

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -26,10 +26,16 @@ class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         return decoratee.loadImageData(from: url) { [weak self] result in
             if let imageData = try? result.get() {
-                self?.cache.save(imageData, for: url) { _ in }
+                self?.cache.saveIgnoringResult(imageData, for: url)
             }
             completion(result)
         }
+    }
+}
+
+extension FeedImageDataCache {
+    func saveIgnoringResult(_ imageData: Data, for url: URL) {
+        save(imageData, for: url) { _ in }
     }
 }
 
