27feeb3283c23ea103f9f2c4d39b03951c9f3627
donataszitkus <zitkusd@gmail.com>
2025-06-29 13:27:50 +0300

    Display FeedImageCellControllers via new 'FeedViewController.display' method

--- EssentialFeed2iOSTests/FeedSnapshotTests.swift
+++ EssentialFeed2iOSTests/FeedSnapshotTests.swift
@@ -11,24 +11,23 @@ import EssentialFeed2iOS
 
 class FeedSnapshotTests: XCTestCase {
     func test_emptyFeed() {
-        let (sut, loader) = makeSUT()
+        let sut = makeSUT()
         
-        loader.completeFeedLoading(with: emptyFeed())
+        sut.display(emptyFeed())
         
         record(snapshot: sut.snapshot(), named: "EMPTY_FEED")
     }
     
     // MARK: - Helpers
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
-        let loader = LoaderSpy()
-        let sut = FeedUIComposer.feedComposedWith(feedLoader: loader, imageLoader: loader)
-        trackForMemoryLeak(loader, file: file, line: line)
-        trackForMemoryLeak(sut, file: file, line: line)
-        sut.loadViewIfNeeded()
-        return (sut, loader)
+    private func makeSUT() -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! FeedViewController
+        controller.loadViewIfNeeded()
+        return controller
     }
     
-    private func emptyFeed() -> [FeedImage] {
+    private func emptyFeed() -> [FeedImageCellController] {
         return []
     }
     
@@ -52,56 +51,6 @@ class FeedSnapshotTests: XCTestCase {
             try snapshotData.write(to: snapshotURL)
         } catch {
             XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
-        }
-    }
-    
-    class LoaderSpy: FeedLoader, FeedImageDataLoader {
-        // MARK: - FeedLoader
-        private var feedRequests = [(FeedLoader.Result) -> Void]()
-        
-        var loadFeedCallCount: Int {
-            return feedRequests.count
-        }
-        
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            feedRequests.append(completion)
-        }
-        
-        func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
-            feedRequests[index](.success(feed))
-        }
-        
-        func completeFeedLoadingWithError(at index: Int) {
-            let error = NSError(domain: "", code: 0)
-            feedRequests[index](.failure(error))
-        }
-        
-        // MARK: - FeedImageDataLoader
-        private struct TaskSpy: FeedImageDataLoaderTask {
-            let cancelCallback: () -> Void
-            func cancel() {
-                cancelCallback()
-            }
-        }
-        
-        private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
-        var loadedImageURLs: [URL] {
-            return imageRequests.map { $0.url }
-        }
-        private(set) var cancelledImageUrls = [URL]()
-        
-        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-            imageRequests.append((url, completion))
-            return TaskSpy { [weak self] in self?.cancelledImageUrls.append(url) }
-        }
-        
-        func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
-            imageRequests[index].completion(.success(imageData))
-        }
-        
-        func completeImageLoadingWithError(at index: Int = 0) {
-            let error = NSError(domain: "error", code: 0)
-            imageRequests[index].completion(.failure(error))
         }
     }
 }
