c03c3677aee0b76c458f99842259b1b46f20d0c4
donataszitkus <zitkusd@gmail.com>
2025-07-04 17:04:57 +0300

    Remove `EssentialFeed2Cache` dependencies from `EssentialFeed2Presentation` framework

--- EssentialFeed2PresentationTests/Helpers/FeedPresentationTestHelpers.swift
+++ EssentialFeed2PresentationTests/Helpers/FeedPresentationTestHelpers.swift
@@ -7,15 +7,11 @@
 
 import Foundation
 import EssentialFeed2
-import EssentialFeed2Cache
 
 func uniqueImage() -> FeedImage {
     return FeedImage(id: UUID(), description: "any", location: "a", url: anyURL())
 }
 
-func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
-    let models = [uniqueImage(), uniqueImage()]
-    let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-    
-    return (models, local)
+func uniqueImageFeed() -> [FeedImage] {
+    return [uniqueImage(), uniqueImage()]
 }
