80ebddfbd93f5b35c7e565907f14e27cd3d9d9d1
donataszitkus <zitkusd@gmail.com>
2025-07-05 13:53:52 +0300

    Make sure that `SceneDelegate` configures window and sets `FeedViewController` as a top view controller

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -6,16 +6,13 @@
 //
 
 import XCTest
+import EssentialFeed2iOS
 @testable import EssentialApp
 
 class SceneDelegateTests: XCTestCase {
     
     func test_configureWindow_setsWindowAsKeyAndVisible() {
-        guard let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
-                XCTFail("No connected UIWindowScene available")
-                return
-            }
-        let window = UIWindow(windowScene: scene)
+        let window = try! makeUIWindow()
         let sut = SceneDelegate()
         sut.window = window
         
@@ -23,6 +20,28 @@ class SceneDelegateTests: XCTestCase {
         
         XCTAssertTrue(window.isKeyWindow, "Expected window to be the key window")
         XCTAssertFalse(window.isHidden, "Expected window to be visible")
+    }
+    
+    func test_sceneWillConnectToSession_configuresRootViewController() {
+        let sut = SceneDelegate()
+        sut.window = try! makeUIWindow()
+        
+        sut.configureWindow()
+        
+        let root = sut.window?.rootViewController
+        let rootNavigation = root as? UINavigationController
+        let topController = rootNavigation?.topViewController
+        
+        XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
+        XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+    
+    // MARK: - Helpers
+    private func makeUIWindow() throws -> UIWindow {
+        guard let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            throw NSError(domain: "WindowSceneError", code: 1, userInfo: [NSLocalizedDescriptionKey: "No connected UIWindowScene available"])
+        }
+        return UIWindow(windowScene: scene)
     }
     
 }
